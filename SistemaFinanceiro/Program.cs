2df9c764382d3a9f11154a98496b285f7d69acc6
drawc <drawc@ART>
2024-05-26 15:27:14 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaFinanceiro/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SistemaFinanceiro.Model;
+
+var conta1 = new Conta(123, 11);
+Console.WriteLine($"Conta: {conta1.Numero}, Saldo: {conta1.Saldo}");
+//conta1.Numero = 321;
+
